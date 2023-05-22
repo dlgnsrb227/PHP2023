41cc656eedb3d00dea73e113d8815c7167e69120
dlgnsrb227 <125418331+dlgnsrb227@users.noreply.github.com>
2023-05-22 15:47:19 +0900

    20230522

--- blog/blogView.php
+++ blog/blogView.php
@@ -8,14 +8,19 @@
         Header("location: blog.php");
     };
 
+    $memberID = $_SESSION['memberID'];
     $blogID = $_GET['blogID'];
 
     $blogSql = "SELECT * FROM blog WHERE blogID = '$blogID'";
     $blogResult = $connect -> query($blogSql);
     $blogInfo = $blogResult -> fetch_array(MYSQLI_ASSOC);
 
+    $commentsql = "SELECT * FROM blogcomment WHERE blogID = '$blogID' AND commentDelete = '0' ORDER BY blogcommentID DESC";
+    $commentResult = $connect -> query($commentsql);
+    $commentInfo = $commentResult -> fetch_array(MYSQLI_ASSOC);
+
     // echo "<pre>";
-    // var_dump($blogInfo);
+    // var_dump($commentInfo);
     // echo "</pre>";
 ?>
 
@@ -72,44 +77,56 @@
         <!-- blog__inner -->
         <div class="blog__comment">
             <h3>댓글</h3>
-            <div class="comment__view">
+
+<?php
+ foreach($commentResult as $comment){?>
+            <div class="comment__view" id="comment<?=$comment['blogCommentID']?>">
                 <div class="avatar">
-                    <img src="https://t1.daumcdn.net/tistory_admin/blog/admin/profile_default_06.png" alt="회원이미지">
+                    <img src="https://t1.daumcdn.net/tistory_admin/blog/admin/profile_default_06.png" alt="">
                 </div>
                 <div class="info">
-                    <span class="nickname">곰돌이</span>
-                    <span class="date">2023.05.22</span>
-                    <p class="msg">댓글 내용이 들어갑니다. 댓글 내용이 들어갑니다. 댓글 내용이 들어갑니다. 댓글 내용이 들어갑니다. 댓글 내용이 들어갑니다. 댓글 내용이 들어갑니다. 댓글 내용이 들어갑니다. 댓글 내용이 들어갑니다. 댓글 내용이 들어갑니다. 댓글 내용이 들어갑니다. 댓글 내용이 들어갑니다. 댓글 내용이 들어갑니다. 댓글 내용이 들어갑니다. 댓글 내용이 들어갑니다. 댓글 내용이 들어갑니다. 댓글 내용이 들어갑니다.</p>
-                    <div class="comment__modify">
-                        <label for="" class="blind">수정내용</label>
-                        <textarea name="msg__modify" id="msg__modify" rows="4" placeholder="수정할 내용을 적어주세요."></textarea>
-                        <label for="" class="blind">비밀번호</label>
-                        <input type="password" id="commentPass" name="commentPass" placeholder="비밀번호를 입력하세요.">
-                        <button id="commentModifyCancle">취소</button>
-                        <button id="commentModifyButton">수정</button>
-                    </div>
-                    <div class="comment__delete">
-                        <label for="commentPass" class="blind">비밀번호</label>
-                        <input type="password" id="commentPass" name="commentPass" placeholder="비밀번호를 입력하세요.">
-                        <button id="commentDeleteCancle">취소</button>
+                    <span class="nickname"><?=$comment['commentName']?></span>
+                    <span class="date"><?=date('y. m. d', $comment['regTime'])?></span>
+                    <p class="msg"><?=$comment['commentMsg']?></p>
+                    <!-- 삭제 -->
+                    <div class="comment__delete" style="display: none">
+                        <label for="commentDeletePass" class="blind">비밀번호</label>
+                        <input type="password" id="commentDeletePass" name="commentDeletePass" placeholder="비밀번호">
+                        <button id="commentDeleteCancel">취소</button>
                         <button id="commentDeleteButton">삭제</button>
                     </div>
-
+                    <!-- //삭제 -->
+                    <!-- 수정 -->
+                    <div class="comment__modify" style="display: none">
+                        <label for="msg__modify" class="blind">수정 내용</label>
+                        <textarea name="msg__modify" id="msg__modify" cols rows="4" placeholder="수정할 내용을 적어주세요!"></textarea>
+                        <label for="commentModifyPass" class="blind">비밀번호</label>
+                        <input type="password" id="commentModifyPass" name="commentModifyPass" placeholder="비밀번호">
+                        <button id="commentModifyCancel">취소</button>
+                        <button id="commentModifyButton">수정</button>
+                    </div>
+                    <!-- //수정 -->
                     <div class="del">
                         <a href="#" class="comment__del__del">삭제</a>
                         <a href="#" class="comment__del__mod">수정</a>
                     </div>
                 </div>
             </div>
+<?php } ?>
 
             <div class="comment__write">
-                <label for="">이름</label>
-                <input type="text" id="commentName" name="commentName" placeholder="이름을 입력하세요.">
-                <label for="commentPass">비밀번호</label>
-                <input type="password" id="commentPass" name="commentPass" placeholder="비밀번호를 입력하세요.">
-                <label for="">댓글쓰기</label>
-                <textarea type="text" id="commentWrite" name="commentWrite" placeholder="댓글을 작성해주세요."></textarea>
-                <button type="submit" class="btnStyle3 mt10">작성</button>
+                <form action="#">
+                    <fieldset>
+                        <legend class="blind">댓글쓰기</legend>
+                        <label for="">이름</label>
+                        <input type="text" id="commentName" name="commentName" placeholder="이름을 입력하세요.">
+                        <label for="commentPass">비밀번호</label>
+                        <input type="password" id="commentPass" name="commentPass" placeholder="비밀번호를 입력하세요." required>
+                        <label for="">댓글쓰기</label>
+                        <textarea type="text" id="commentWrite" name="commentWrite" placeholder="댓글을 작성해주세요." maxlength="255" required></textarea>
+                        <button type="button" id="commentWriteBtn" class="btnStyle3 mt10">작성</button>
+                    </fieldset>
+                </form>
             </div>
         </div>
         <!-- blog__comment -->
@@ -136,6 +153,87 @@
 
     <?php include "../include/footer.php" ?>
     <!-- footer -->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script>
+        let commentID ="";
+
+        // 댓글 삭제 버튼
+        $(".comment__del__del").click(function(e){
+            e.preventDefault();
+            $(this).parent().prev().prev().show();
+
+            commentID = $(this).parent().parent().parent().attr("id");
+        });
+
+        // 댓글 삭제 취소버튼
+        $("#commentDeleteCancel").click(function(){
+            $(".comment__delete").hide();
+        });
+
+        // 댓글 삭제 삭제버튼
+        $("#commentDeleteButton").click(function(){
+            let number = commentID.replace(/[^0-9]/g, "");
+            if($("#commentDeletePass").val() == ""){
+                alert("비밀번호를 입력해주세요");
+                $("#commentDeletePass").focus();
+            } else {
+                $.ajax({
+                    url : "blogCommentDelete.php",
+                    method : "POST",
+                    dataType : "json",
+                    data : {
+                        "commentPass" : $("#commentDeletePass").val(),
+                        "commentID" : number,
+                    },
+                    success : function(data){
+                        if(data.result == "good"){
+                            alert("댓글이 삭제되었습니다.")
+                            location.reload();
+                        } else {
+                            alert("비밀번호가 틀렸습니다.")
+                            location.reload();
+                        }
+                    },
+                    error : function(request, status, error){
+                        console.log("request" + request);
+                        console.log("status" + status);
+                        console.log("error" + error);
+                    }
+                });
+            };
+        });
+
+        // 댓글 쓰기 버튼
+        $("#commentWriteBtn").click(function(){
+            if($("#commentWrite").val() == ""){
+                alert("댓글을 작성해주세요.");
+                $("#commentWrite").focus();
+            } else {
+                $.ajax({
+                    url : "blogCommentWrite.php",
+                    method : "POST",
+                    dataType : "json",
+                    data : {
+                        "blogID": <?=$blogID?>,
+                        "memberID": <?=$memberID?>,
+                        "name" : $("#commentName").val(),
+                        "pass" : $("#commentPass").val(),
+                        "msg" : $("#commentWrite").val(),
+                    },
+                    success : function(data){
+                        console.log(data);
+                        location.reload();
+                    },
+                    error : function(request, status, error){
+                        console.log("request" + request);
+                        console.log("status" + status);
+                        console.log("error" + error);
+                    }
+                });
+            }
+        });
+    </script>
 
 </body>
 </html>
